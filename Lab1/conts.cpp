75e5da6f3046ca8a97ed6e1c87b625db8c8e8680
kakrulit <sav1920@mail.ru>
2023-09-24 21:52:17 +0300

    update

--- Lab1/conts.cpp
+++ Lab1/conts.cpp
@@ -10,7 +10,7 @@ int getRandNum(int min, int max) {
 	return min + rand() % (max - min + 1);
 }
 
-void fillMatrix(vector<vector<int>> &matrix, const int A, const int B)
+void fillMatrix(vector<vector<int>>& matrix, const int A, const int B)
 {
 	for (int i = 0; i < A; i++)
 	{
@@ -21,112 +21,61 @@ void fillMatrix(vector<vector<int>> &matrix, const int A, const int B)
 	}
 }
 
-void showMatrix(vector<vector<int>>& matrix, const int A, const int B)
+void swapOddRows(vector<vector<int>>& matrix, const int start, const int end)
 {
-	for (int i = 0; i < A; i++)
-	{
-		for (int j = 0; j < B; j++)
-		{
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-}
-
-void swapOddRows(vector<vector<int>> &matrix, const int A)
-{
-	for (int i = 0; i < A / 2; i++)
+	for (int i = start; i < end / 2; i++)
 	{
 		if (i % 2 == 0)
 		{
-			swap(matrix[i], matrix[A - i - 1]);
+			swap(matrix[i], matrix[matrix.size() - i - 1]);
 		}
 	}
 }
 
-void fourThreadTest()
+void test()
 {
-	int A = getRandNum(2000, 3000);
-	int B = getRandNum(2000, 3000);
+	vector<int> numThreads = { 1, 2, 4 };
 
-	vector<vector<int>> matrix(A, vector<int>(B, 0));
-
-	fillMatrix(matrix, A, B);
-
-	vector<thread> threads(4);
-
-	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-	for (int i = 0; i < 4; i++)
+	for (auto& el : numThreads)
 	{
-		threads[i] = std::thread(swapOddRows, std::ref(matrix), A / 4);
+		int A = getRandNum(2000, 3000);
+		int B = getRandNum(2000, 3000);
+
+		vector<vector<int>> matrix(A, vector<int>(B, 0));
+
+		fillMatrix(matrix, A, B);
+
+		vector<thread> threads(el);
+
+		int chunkSize = A / el;
+
+		chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
+
+		for (int i = 0; i < el; i++)
+		{
+			int start = i * chunkSize;
+			int end = (i == el - 1) ? A : (i + 1) * chunkSize;
+			threads[i] = std::thread(swapOddRows, std::ref(matrix), start, end);
+		}
+
+		for (int i = 0; i < el; i++)
+		{
+			threads[i].join();
+		}
+
+		chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
+
+		chrono::duration<double> sec_diff = end - start;
+
+		cout << "Time: " << sec_diff.count() << " sec." << endl;
 	}
-
-	for (int i = 0; i < 4; ++i)
-	{
-		threads[i].join();
-	}
-
-	chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
-
-	chrono::duration<double> sec_diff = end - start;
-
-	cout << "Time: " << sec_diff.count() << " sec." << endl;
-}
-
-void twoThreadTest()
-{
-	int A = getRandNum(2000, 3000);
-	int B = getRandNum(2000, 3000);
-
-	vector<vector<int>> matrix(A, vector<int>(B, 0));
-
-	fillMatrix(matrix, A, B);
-
-	vector<thread> threads(2);
-
-	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-	for (int i = 0; i < 2; i++)
-	{
-		threads[i] = std::thread(swapOddRows, std::ref(matrix), A / 2);
-	}
-
-	for (int i = 0; i < 2; ++i)
-	{
-		threads[i].join();
-	}
-
-	chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
-
-	chrono::duration<double> sec_diff = end - start;
-
-	cout << "Time: " << sec_diff.count() << " sec." << endl;
-}
-
-void oneThreadTest()
-{
-	int A = getRandNum(2000, 3000);
-	int B = getRandNum(2000, 3000);
-
-	vector<vector<int>> matrix(A, vector<int>(B, 0));
-
-	fillMatrix(matrix, A, B);
-
-	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-	swapOddRows(matrix, A);
-	chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
-
-	chrono::duration<double, milli> milli_diff = end - start;
-
-	cout <<"Time: " << milli_diff.count() << " milli sec." << endl;
 }
 
 int main()
 {
 	srand(time(NULL));
 
-	oneThreadTest();
-	twoThreadTest();
-	fourThreadTest();
+	test();
 
 	return 0;
 }
